5e45c214e6c2015338e94e0822461e20fdece535
rfmoreira <rafamor2@gmail.com>
2020-09-08 18:40:39 -0300

    Criação de pagína inicial e Barra de navegação

--- app/src/main/java/com/example/grandson/View/LoginGrandson.java
+++ app/src/main/java/com/example/grandson/View/LoginGrandson.java
@@ -2,16 +2,31 @@ package com.example.grandson.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.grandson.R;
 
 public class LoginGrandson extends AppCompatActivity {
 
+    private Button btLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_grandson);
+
+        btLogin = (Button) findViewById(R.id.btLogin);
+
+        btLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),HomeCliente.class);
+                startActivity(intent);
+            }
+        });
     }
 
 }
